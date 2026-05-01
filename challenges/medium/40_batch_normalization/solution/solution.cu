f2d8e6cc14f98425221e7474b731a1e429c2f106
amos <xingdiyou@gmail.com>
2026-05-01 11:27:35 +0800

    Add MathJax script and CUDA solution for challenge 74, optimize challenge 40
    - Add MathJax script to challenge 74 layer normalization
    - Add CUDA solution for challenge 74 using CUB BlockLoad/BlockReduce
    - Optimize challenge 40 batch normalization by fusing sum and square_sum
      reduction into a single float2 BlockReduce

--- challenges/medium/40_batch_normalization/solution/solution.cu
+++ challenges/medium/40_batch_normalization/solution/solution.cu
@@ -1,10 +1,16 @@
 #include <cub/cub.cuh>
 #include <cuda_runtime.h>
 
+struct ReduceOp {
+    __device__ float2 operator()(float2 a, float2 b) const {
+        return make_float2(a.x + b.x, a.y + b.y);
+    }
+};
+
 template <int BlockSize>
 __global__ void batch_norm_kernel(const float* input, const float* gamma, const float* beta,
                                   float* output, int N, int C, float eps) {
-    using BlockReduce = cub::BlockReduce<float, BlockSize>;
+    using BlockReduce = cub::BlockReduce<float2, BlockSize>;
 
     __shared__ typename BlockReduce::TempStorage temp_storage;
 
@@ -16,13 +22,14 @@ __global__ void batch_norm_kernel(const float* input, const float* gamma, const 
         square_sum += x * x;
     }
 
-    sum = BlockReduce(temp_storage).Sum(sum);
-    __syncthreads();
-    square_sum = BlockReduce(temp_storage).Sum(square_sum);
+    float2 sum_sq_sum = make_float2(sum, square_sum);
+    float2 block_sum_sq_sum = BlockReduce(temp_storage).Reduce(sum_sq_sum, ReduceOp());
 
     __shared__ float mean;
     __shared__ float inv_std;
     if (threadIdx.x == 0) {
+        float sum = block_sum_sq_sum.x;
+        float square_sum = block_sum_sq_sum.y;
         mean = sum / N;
         float variance = square_sum / N - mean * mean;
         inv_std = rsqrtf(variance + eps);
