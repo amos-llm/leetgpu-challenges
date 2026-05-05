176db7f4e7cb03987a9635a5fac168263510f999
amos <xingdiyou@gmail.com>
2026-05-05 09:50:23 +0800

    Refactor CUDA solution for challenge 1: use CUB BlockLoad and BlockStore

--- challenges/easy/1_vector_add/solution/solution.cu
+++ challenges/easy/1_vector_add/solution/solution.cu
@@ -1,18 +1,35 @@
+#include <cub/cub.cuh>
 #include <cuda_runtime.h>
 
-__global__ void vector_add(const float* A, const float* B, float* C, int N) {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx >= N) {
-        return;
+template <int BlockSize, int ItemsPerThread>
+__global__ void add_vector_kernel(const float* A, const float* B, float* C, int N) {
+    using BlockLoad = cub::BlockLoad<float, BlockSize, ItemsPerThread, cub::BLOCK_LOAD_VECTORIZE>;
+    using BlockStore =
+        cub::BlockStore<float, BlockSize, ItemsPerThread, cub::BLOCK_STORE_VECTORIZE>;
+
+    __shared__ union {
+        typename BlockLoad::TempStorage load;
+        typename BlockStore::TempStorage store;
+    } temp_storage;
+
+    int block_offset = blockIdx.x * BlockSize * ItemsPerThread;
+    float a_items[ItemsPerThread];
+    float b_items[ItemsPerThread];
+    BlockLoad(temp_storage.load).Load(A + block_offset, a_items, N - block_offset);
+    BlockLoad(temp_storage.load).Load(B + block_offset, b_items, N - block_offset);
+#pragma unroll
+    for (int i = 0; i < ItemsPerThread; ++i) {
+        a_items[i] += b_items[i];
     }
-    C[idx] = A[idx] + B[idx];
+    BlockStore(temp_storage.store).Store(C + block_offset, a_items, N - block_offset);
 }
 
 // A, B, C are device pointers (i.e. pointers to memory on the GPU)
 extern "C" void solve(const float* A, const float* B, float* C, int N) {
-    int threadsPerBlock = 256;
-    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
-
-    vector_add<<<blocksPerGrid, threadsPerBlock>>>(A, B, C, N);
+    constexpr int kBlockSize = 256;
+    constexpr int kItemsPerThread = 4;
+    constexpr int kItemsPerBlock = kBlockSize * kItemsPerThread;
+    int grid_size = (N + kItemsPerBlock - 1) / kItemsPerBlock;
+    add_vector_kernel<kBlockSize, kItemsPerThread><<<grid_size, kBlockSize>>>(A, B, C, N);
     cudaDeviceSynchronize();
 }
