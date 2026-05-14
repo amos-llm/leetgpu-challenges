a7c474d91879a1f98a39e7e96d709a3d2ce642be
amos <xingdiyou@gmail.com>
2026-05-14 09:28:16 +0800

    Add CUDA solution for challenge 98: beam search step

---
+++ challenges/medium/98_beam_search_step/solution/solution.cu
@@ -0,0 +1,141 @@
+#include <cuda_runtime.h>
+#include <float.h>
+
+template <int K>
+__device__ void insert_topk(float* scores, int* tokens, int* parents, float score, int token,
+                            int parent) {
+#pragma unroll
+    for (int i = 0; i < K; ++i) {
+        if (score > scores[i]) {
+#pragma unroll
+            for (int j = K - 1; j > i; --j) {
+                scores[j] = scores[j - 1];
+                tokens[j] = tokens[j - 1];
+                parents[j] = parents[j - 1];
+            }
+            scores[i] = score;
+            tokens[i] = token;
+            parents[i] = parent;
+            return;
+        }
+    }
+}
+
+template <int K>
+__device__ void merge_topk(float* dst_scores, int* dst_tokens, int* dst_parents,
+                           const float* src_scores, const int* src_tokens, const int* src_parents) {
+#pragma unroll
+    for (int i = 0; i < K; ++i) {
+        insert_topk<K>(dst_scores, dst_tokens, dst_parents, src_scores[i], src_tokens[i],
+                       src_parents[i]);
+    }
+}
+
+template <int K> __device__ void reduce_topk(float* scores, int* tokens, int* parents) {
+    int lane_id = threadIdx.x % 32;
+#pragma unroll
+    for (int s = 16; s > 0; s /= 2) {
+        float other_scores[K];
+        int other_tokens[K];
+        int other_parents[K];
+#pragma unroll
+        for (int i = 0; i < K; ++i) {
+            other_scores[i] = __shfl_down_sync(0xffffffff, scores[i], s);
+            other_tokens[i] = __shfl_down_sync(0xffffffff, tokens[i], s);
+            other_parents[i] = __shfl_down_sync(0xffffffff, parents[i], s);
+        }
+        if (lane_id < s) {
+            merge_topk<K>(scores, tokens, parents, other_scores, other_tokens, other_parents);
+        }
+    }
+}
+
+template <int K>
+__global__ void beam_search_step_kernel(const float* __restrict__ beam_scores,    // [B, K]
+                                        const float* __restrict__ token_logprobs, // [B, K, V]
+                                        float* __restrict__ new_beam_scores,      // [B, K]
+                                        int* __restrict__ parent_beam_indices,    // [B, K]
+                                        int* __restrict__ next_tokens,            // [B, K]
+                                        int V) {
+    int batch_idx = blockIdx.x;
+    int lane_id = threadIdx.x % 32;
+    int warp_id = threadIdx.x / 32;
+
+    float top_scores[K];
+    int top_tokens[K];
+    int top_parents[K];
+#pragma unroll
+    for (int i = 0; i < K; ++i) {
+        top_scores[i] = -FLT_MAX;
+        top_tokens[i] = -1;
+        top_parents[i] = -1;
+    }
+
+    int total_candidates = K * V;
+    for (int i = threadIdx.x; i < total_candidates; i += blockDim.x) {
+        int beam_idx = i / V;
+        int token_idx = i % V;
+        float score = beam_scores[batch_idx * K + beam_idx] +
+                      token_logprobs[batch_idx * K * V + beam_idx * V + token_idx];
+        insert_topk<K>(top_scores, top_tokens, top_parents, score, token_idx, beam_idx);
+    }
+
+    reduce_topk<K>(top_scores, top_tokens, top_parents);
+
+    __shared__ float shared_scores[32 * K];
+    __shared__ int shared_tokens[32 * K];
+    __shared__ int shared_parents[32 * K];
+    if (lane_id == 0) {
+        for (int i = 0; i < K; ++i) {
+            shared_scores[warp_id * K + i] = top_scores[i];
+            shared_tokens[warp_id * K + i] = top_tokens[i];
+            shared_parents[warp_id * K + i] = top_parents[i];
+        }
+    }
+    __syncthreads();
+
+    if (warp_id == 0) {
+        float final_scores[K];
+        int final_tokens[K];
+        int final_parents[K];
+#pragma unroll
+        for (int i = 0; i < K; ++i) {
+            final_scores[i] = shared_scores[lane_id * K + i];
+            final_tokens[i] = shared_tokens[lane_id * K + i];
+            final_parents[i] = shared_parents[lane_id * K + i];
+        }
+
+        reduce_topk<K>(final_scores, final_tokens, final_parents);
+
+        if (lane_id == 0) {
+#pragma unroll
+            for (int i = 0; i < K; ++i) {
+                new_beam_scores[batch_idx * K + i] = final_scores[i];
+                next_tokens[batch_idx * K + i] = final_tokens[i];
+                parent_beam_indices[batch_idx * K + i] = final_parents[i];
+            }
+        }
+    }
+}
+
+extern "C" void solve(const float* beam_scores, const float* token_logprobs, float* new_beam_scores,
+                      int* parent_beam_indices, int* next_tokens, int B, int K, int V) {
+    constexpr int kBlockSize = 1024;
+#define DISPATCH_KERNEL(CASE_K)                                                                    \
+    case CASE_K:                                                                                   \
+        beam_search_step_kernel<CASE_K><<<B, kBlockSize>>>(                                        \
+            beam_scores, token_logprobs, new_beam_scores, parent_beam_indices, next_tokens, V);    \
+        break;
+    switch (K) {
+        DISPATCH_KERNEL(1)
+        DISPATCH_KERNEL(2)
+        DISPATCH_KERNEL(3)
+        DISPATCH_KERNEL(4)
+        DISPATCH_KERNEL(5)
+        DISPATCH_KERNEL(6)
+        DISPATCH_KERNEL(7)
+        DISPATCH_KERNEL(8)
+    }
+#undef DISPATCH_KERNEL
+    cudaDeviceSynchronize();
+}
